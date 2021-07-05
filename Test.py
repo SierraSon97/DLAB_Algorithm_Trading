83c8ca09ac809a6a585203c1c5537b50c5b1d51d
swj1265 <swj1265@naver.com>
2021-07-06 01:43:11 +0900

    20210706

--- Test.py
+++ Test.py
@@ -1,8 +1,3 @@
 import win32com.client
-
-objCpCybos = win32com.client.Dispatch("CpUtil.CpCybos")
-bConnect = objCpCybos.IsConnect
-
-if(bConnect == 0):
-    print("PLUS가 정상적으로 연결되지 않음. ")
-    exit()
+instCpCybos = win32com.client.Dispatch("CpUtil.CpCybos")
+print(instCpCybos.IsConnect)
