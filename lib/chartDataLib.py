05fa25f6ebe0d9863900c4745935e6914999e600
swj1265 <swj1265@naver.com>
2021-08-03 13:25:29 +0900

    20210803

--- lib/chartDataLib.py
+++ lib/chartDataLib.py
@@ -17,65 +17,73 @@ objStockChart = win32com.client.Dispatch("CpSysDib.StockChart")
 # 현재가 객체 구하기
 objStockMst = win32com.client.Dispatch("DsCbo1.StockMst")
 
+instMarketEye = win32com.client.Dispatch("CpSysDib.MarketEye")
+class getChartData():
+    def getObjStock(self, stockcode):
+        objStockMst.SetInputValue(0, stockcode)
+        objStockMst.BlockRequest()
 
-def getObjStock(stockCode):
-    objStockMst.SetInputValue(0, stockCode)
-    objStockMst.BlockRequest()
+    def findStock(self, stockname):
+        for i in range(stockNum):
+            if instCpStockCode.GetData(1, i) == stockname:
+                return instCpStockCode.GetData(0, i)
+        return 0
 
-def getCodeList():
-    return
+    def setInputValue(self, stockcode):
+        objStockChart.SetInputValue(0, stockcode)  # 종목 코드
+        objStockChart.SetInputValue(1, ord('2'))  # 개수로 조회
+        objStockChart.SetInputValue(4, 100)  # 최근 100일 치
+        objStockChart.SetInputValue(5, [0, 2, 3, 4, 5, 8])  # 날짜,시가,고가,저가,종가,거래량
+        objStockChart.SetInputValue(6, ord('D'))  # '차트 주가 - 일간 차트 요청
+        objStockChart.SetInputValue(9, ord('1'))  # 수정주가 사용
+        objStockChart.BlockRequest()
 
-def findStock(stockName):
-    for i in range(stockNum):
-        if instCpStockCode.GetData(1, i) == stockName:
-            stockCode = instCpStockCode.GetData(0, i)
-            return stockCode
-    return 0
+    def setInputValue(self, stockcode, startdate, enddate):
+        objStockChart.SetInputValue(0, stockcode)  # 종목 코드
+        objStockChart.SetInputValue(1, ord('2'))  # 개수로 조회
+        objStockChart.SetInputValue(2, startdate)
+        objStockChart.SetInputValue(3, enddate)
+        objStockChart.SetInputValue(4, 100)  # 최근 100일 치
+        objStockChart.SetInputValue(5, [0, 2, 3, 4, 5, 8])  # 날짜,시가,고가,저가,종가,거래량
+        objStockChart.SetInputValue(6, ord('D'))  # '차트 주가 - 일간 차트 요청
+        objStockChart.SetInputValue(9, ord('1'))  # 수정주가 사용
+        objStockChart.BlockRequest()
 
-def setInputValue(stockCode):
-    objStockChart.SetInputValue(0, stockCode)  # 종목 코드
-    objStockChart.SetInputValue(1, ord('2'))  # 개수로 조회
-    objStockChart.SetInputValue(4, 100)  # 최근 100일 치
-    objStockChart.SetInputValue(5, [0, 2, 3, 4, 5, 8])  # 날짜,시가,고가,저가,종가,거래량
-    objStockChart.SetInputValue(6, ord('D'))  # '차트 주가 - 일간 차트 요청
-    objStockChart.SetInputValue(9, ord('1'))  # 수정주가 사용
-    objStockChart.BlockRequest()
+    def getDataValue(self):
+        len = objStockChart.GetHeaderValue(3)
 
-def setInputValue(stockCode, startDate, endDate):
-    objStockChart.SetInputValue(0, stockCode)  # 종목 코드
-    objStockChart.SetInputValue(1, ord('2'))  # 개수로 조회
-    objStockChart.SetInputValue(2, startDate)
-    objStockChart.SetInputValue(3, endDate)
-    objStockChart.SetInputValue(4, 100)  # 최근 100일 치
-    objStockChart.SetInputValue(5, [0, 2, 3, 4, 5, 8])  # 날짜,시가,고가,저가,종가,거래량
-    objStockChart.SetInputValue(6, ord('D'))  # '차트 주가 - 일간 차트 요청
-    objStockChart.SetInputValue(9, ord('1'))  # 수정주가 사용
-    objStockChart.BlockRequest()
+        day = []
+        open = []
+        high = []
+        low = []
+        close = []
+        vol = []
 
+        for i in range(len):
+            day.append(objStockChart.GetDataValue(0, i))
+            open.append(objStockChart.GetDataValue(1, i))
+            high.append(objStockChart.GetDataValue(2, i))
+            low.append(objStockChart.GetDataValue(3, i))
+            close.append(objStockChart.GetDataValue(4, i))
+            vol.append(objStockChart.GetDataValue(5, i))
+        df = pd.DataFrame({
+            'Day' : day,
+            'Open' : open,
+            'High' : high,
+            'Low' : low,
+            'Close' : close,
+            'Vol' : vol
+        })
+        return df
 
-def getDataValue():
-    len = objStockChart.GetHeaderValue(3)
+class getPERData():
+    def SetInputValue(self, stockcode):
+        instMarketEye.SetInputValue(0, (4, 67, 70, 111))
+        instMarketEye.SetInputValue(1, stockcode)
+        instMarketEye.BlockRequest()
 
-    day = []
-    open = []
-    high = []
-    low = []
-    close = []
-    vol = []
-
-    for i in range(len):
-        day.append(objStockChart.GetDataValue(0, i))
-        open.append(objStockChart.GetDataValue(1, i))
-        high.append(objStockChart.GetDataValue(2, i))
-        low.append(objStockChart.GetDataValue(3, i))
-        close.append(objStockChart.GetDataValue(4, i))
-        vol.append(objStockChart.GetDataValue(5, i))
-    df = pd.DataFrame({
-        'Day' : day,
-        'Open' : open,
-        'High' : high,
-        'Low' : low,
-        'Close' : close,
-        'Vol' : vol
-    })
-    return df
+    def GetData(self):
+        print("현재가: ", instMarketEye.GetDataValue(0, 0))
+        print("PER: ", instMarketEye.GetDataValue(1, 0))
+        print("EPS: ", instMarketEye.GetDataValue(2, 0))
+        print("최근분기년월: ", instMarketEye.GetDataValue(3, 0))
